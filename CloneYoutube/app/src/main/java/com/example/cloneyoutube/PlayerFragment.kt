ce829c5216276c9b70b300be3df1ee2c1fb2756a
9mean <kanggm102@naver.com>
2022-07-28 15:00:30 +0900

    exoplayer를 이용하여 동영상 재생시키기

--- CloneYoutube/app/src/main/java/com/example/cloneyoutube/PlayerFragment.kt
+++ CloneYoutube/app/src/main/java/com/example/cloneyoutube/PlayerFragment.kt
@@ -1,5 +1,6 @@
 package com.example.cloneyoutube
 
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import androidx.constraintlayout.motion.widget.MotionLayout
@@ -9,6 +10,11 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.cloneyoutube.adapter.VideoAdapter
 import com.example.cloneyoutube.databinding.FragmentPlayerBinding
 import com.example.cloneyoutube.service.VideoService
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 import kotlinx.coroutines.launch
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -17,12 +23,15 @@ import kotlin.math.abs
 class PlayerFragment: Fragment(R.layout.fragment_player) {
     private var binding:FragmentPlayerBinding?=null
     private lateinit var videoAdapter: VideoAdapter
+    private var player:SimpleExoPlayer?=null
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val fragmentPlayerBinding=FragmentPlayerBinding.bind(view)
         binding=fragmentPlayerBinding
         initMotionLayoutEvent(fragmentPlayerBinding)
         initRecyclerView(fragmentPlayerBinding)
+        initPlayer(fragmentPlayerBinding)
+        initControlBtn(fragmentPlayerBinding)
         getViedoList()
     }
     private fun initMotionLayoutEvent(fragmentPlayerBinding: FragmentPlayerBinding){
@@ -73,6 +82,35 @@ class PlayerFragment: Fragment(R.layout.fragment_player) {
             layoutManager=LinearLayoutManager(context)
         }
     }
+    private fun initPlayer(fragmentPlayerBinding: FragmentPlayerBinding){
+        context?.let{
+
+            player=SimpleExoPlayer.Builder(it).build()
+        }
+        fragmentPlayerBinding.playerView.player=player
+        binding?.let{
+            player?.addListener(object : Player.EventListener {
+                override fun onIsPlayingChanged(isPlaying: Boolean) {
+                    super.onIsPlayingChanged(isPlaying)
+                    if(isPlaying){
+                        it.bottomPlayerControllerBtn.setImageResource(R.drawable.ic_baseline_pause_24)
+                    }else{
+                        it.bottomPlayerControllerBtn.setImageResource(R.drawable.ic_baseline_play_arrow_24)
+                    }
+                }
+            })
+        }
+    }
+    private fun initControlBtn(fragmentPlayerBinding: FragmentPlayerBinding){
+        fragmentPlayerBinding.bottomPlayerControllerBtn.setOnClickListener {
+            val player=this.player?:return@setOnClickListener
+            if(player.isPlaying){
+                player.pause()
+            }else{
+                player.play()
+            }
+        }
+    }
     private fun getViedoList(){
         val retrofit= Retrofit.Builder()
             .baseUrl("https://run.mocky.io/")
@@ -90,13 +128,27 @@ class PlayerFragment: Fragment(R.layout.fragment_player) {
         }
     }
     fun play(url:String,title:String){
+        context?.let {
+            val dataSourceFactory=DefaultDataSourceFactory(it)
+            val mediaSource=ProgressiveMediaSource.Factory(dataSourceFactory)
+                .createMediaSource(MediaItem.fromUri(Uri.parse(url)))
+            player?.setMediaSource(mediaSource)
+            player?.prepare()
+            player?.play()
+        }
         binding?.let {
             it.playerMotionLayout.transitionToEnd()
             it.bottomTitleTextView.text=title
         }
     }
+
+    override fun onStop() {
+        super.onStop()
+        player?.pause()
+    }
     override fun onDestroy() {
         super.onDestroy()
         binding=null
+        player?.release()
     }
 }
